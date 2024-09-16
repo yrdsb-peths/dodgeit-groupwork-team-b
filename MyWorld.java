0d56059fc6b355807bed2b5b77237c65ae341508
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-09-16 12:52:57 -0400

    Revert "Add a hero and put skull on the screen"
    This reverts commit 755d58b27d95d724d9126dc7b8b0d5ff70511901.

--- MyWorld.java
+++ MyWorld.java
@@ -17,10 +17,5 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        GameOver gameover = new GameOver();
-        Hero hero = new Hero();
-        
-        addObject(hero, 100, 100);
-        addObject(gameover, 300, 200);
     }
 }
